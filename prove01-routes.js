7591165b9d1df37ba7c9ddf90dee52705762f9ed
DParkinson-BYUI <parkidr@gmail.com>
2021-04-24 18:54:09 -0700

    update prove01 to use express and ejs

--- prove01-routes.js
+++ prove01-routes.js
@@ -1,53 +1,37 @@
+const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 
-const requestHandler = (req, res) => {
-    const url = req.url;
-    const method = req.method;
+//let userInputArray  = ["User 1", "User 2", "User 3"];
+//let userList = ["User 1", "User 2", "User 3"];
 
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Prove 01</title></head>');        
-        res.write('<body><h1>Welcome visitor!</h1><p>Please enter a username:</p><form action="/create-user" method="POST"><label for="username">UserName:<input type="text" name="username" required></label><button type="submit">Submit username</button></form></body>');
-        res.write('</html>');
-        return res.end();        
-    }
-    
-    if (url === '/create-user' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {            
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const username = parseBody.split('=')[1];            
-            console.log(username);
-            const userList = JSON.parse(fs.readFileSync('userList.json'));
-            userList.push(username);
-            fs.writeFileSync('userList.json', JSON.stringify(userList));            
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
-        });       
-    }
+router.get('/', (req, res, next) => {
+    res.render('pages/prove01/prove01', {
+        title: 'Prove 01 Assignment',
+        path: '/prove01'
+    })
+});
 
-    if (url === '/users') {
-        const userList = JSON.parse(fs.readFileSync('userList.json'));
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Prove 01</title></head>');
-        //res.write('<body><ul>'+userList.map(u=>`<li>${u}</li>`).join('\n')+'</ul></body>');
-        res.write(htmlWrap('Prove 1', '<ul>'+userList.map(u=>`<li>${u}</li>`).join('\n')+'</ul>'));
-        res.write('</html>');        
-        return res.end();       
-    }    
+router.get('/users', (req, res, next) => {
+    const userList = JSON.parse(fs.readFileSync('userList.json'));
+    res.render('pages/prove01/allusers', {
+        title: 'Prove 01 Assignment',
+        path: '/prove01',
+        userData: userList
+    })
+});
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My Prove01 Page</title></head>');
-    res.write('<body><h1>Something is wrong, no one should be here!</h1></body>');
-    res.write('</html>');
-    res.end();    
-};
+router.post('/create-user', async (req, res, next) => {
+    // don't need body because the username info is already stored in req
+    // extract data from req.body.username
+    const user = req.body.username;
+    const userList = JSON.parse(fs.readFileSync('userList.json'));
+    userList.push(user);
+    fs.writeFileSync('userList.json', JSON.stringify(userList));
+    console.log(userList);
+    // keep user on the original page
+    res.redirect('/');
 
-module.exports.handler = requestHandler;
+})
+
+module.exports = router;
--- prove01.js
+++ prove01.js
@@ -1,8 +1,23 @@
-const http = require('http');
-const routes = require('./prove01-routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 3000 // So we can run on heroku || (OR) localhost:3000
 
-// Create the nodejs Server
-const server = http.createServer(routes.handler);
+const app = express();
+const prove01Routes = require('./prove01-routes');
 
-// Listen on port 3000
-server.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')))
+   .set('views', path.join(__dirname, 'views'))
+   .set('view engine', 'ejs')
+   // For view engine as Pug
+   //.set('view engine', 'pug') // For view engine as PUG. 
+   // For view engine as hbs (Handlebars)
+   //.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})) // For handlebars
+   //.set('view engine', 'hbs')
+   .use(express.urlencoded({extended: false})) // For parsing the body of a POST
+   .use('/', prove01Routes)
+   .use((req, res, next) => {
+     // 404 page
+     res.render('pages/404', {title: '404 - Page Not Found', path: req.url})
+   })
+   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
